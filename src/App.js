999455525dcd216dcf7fc37810a11b07ed88651e
koyanabn <koyana.singh@bntechnology.net>
2024-01-19 11:57:00 +0530

    comments

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; //react hook, allows us to track state in a function component
 
 function App() {
   // State to store the fetched data
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null); //initial state of useState is 'null' (by default) 
+  //then it will be 'data' , 'setData' is the function which will update the state of useState
 
-  // Function to fetch data from the server
-  const fetchData = async () => {
+  const fetchData = async () => { //function to fetch data from the server
     try {
       const response = await fetch('https://wild-cyan-bream.cyclic.app/api/data');
-      const jsonData = await response.json();
+      //fetch starts a request and returns a promise
+      //if the request gets completed, the promise is resolved with the Response object
+      //if the request fails, the promise is rejected.
+      const jsonData = await response.json(); //returns a promise resolved to a JSON object
 
-      // Update state with the fetched data
-      setData(jsonData);
+      setData(jsonData); //update state with the fetched data
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error); //if it gets error, the error will be handled here
     }
   };
 
   return (
     <div>
-      <h1>React App with Button</h1>
+      <h1>API trial using cyclic and Netlify</h1>
       <button onClick={fetchData}>Fetch Data</button>
-
+      {/* function call to fetch data */}
       {data && (
         <div>
           <h2>Fetched Data:</h2>
           <pre>{JSON.stringify(data, null, 2)}</pre>
+          {/* When sending data to a web server, the data has to be a string.
+          Convert a JavaScript object into a string with JSON.stringify(). */}
         </div>
       )}
     </div>
@@ -33,3 +37,4 @@ function App() {
 }
 
 export default App;
+// it's used to export functions, objects, classes or expressions from script files or modules
